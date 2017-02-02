88ed0e71e0e7a2051c125144bae9afbcf905f3fb
paradon_mac <paradon@wisdomlanna.com>
2017-02-02 22:34:07 +0700

    Change check duplicate file package and put cmd color when you got success or error message when generate file

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require('gulp-param')(require('gulp'), process.argv),
     // uglify = require('gulp-uglify'),
     // del = require('del'),
     replace = require('gulp-replace'),
-    fs = require('fs'),
+    fileExists = require('file-exists'),
+    chalk = require('chalk'),
     rename = require("gulp-rename");
 
 gulp.task('make', function (controller, model) {
@@ -13,20 +14,17 @@ gulp.task('make', function (controller, model) {
     const indexChar = make.indexOf("/");
     const file = make.substr(indexChar + 1);
     const folder = make.substr(0, indexChar);
-
     const extraFile = makeStr + 's/' + make + '.js';
 
-    fs.stat('extraFile', function (err, stat) {
-        if (err || !stat) {
-            console.log(make + ' already exist! Pls,try other name');
-        } else {
-            gulp.src(['core/blueprint/' + makeStr + '.js'])
-                .pipe(rename(file + '.js'))
-                .pipe(replace(makeStr.toUpperCase(), file))
-                .pipe(gulp.dest(makeStr + 's/' + folder));
-            console.log('Generate ' + makeStr + ' Success');
-        }
-    });
+    if (fileExists(extraFile)) {
+        console.log(chalk.red('Error: ' + make + ' already exist! Pls,try other name'));
+    } else {
+        gulp.src(['core/blueprint/' + makeStr + '.js'])
+            .pipe(rename(file + '.js'))
+            .pipe(replace(makeStr.toUpperCase(), file))
+            .pipe(gulp.dest(makeStr + 's/' + folder));
+        console.log(chalk.green('Generate ' + makeStr + ' Success'));
+    }
 });
 
 gulp.task('serve', function () {
